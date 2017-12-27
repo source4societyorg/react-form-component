16606a566b18b2548bfa61dc693d8be7fee070d4
Daniel Fredriksen <dfredriksen@cyint.technology>
2017-12-27 07:21:13 +0000

    Improved select

--- Select.js
+++ Select.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Select = ({value: value, isValid: isValid, errorColor: errorColor, onChange: onChange, options:options, ...props}) => (
-    <select {...props} onChange={onChange} defaultValue={value}>
-      {renderOptions(options.toJS(), value)}
-    </select>
+const Select = ({value: value, isValid: isValid, fieldData: feldData, text: text, errorColor: errorColor, onChange: onChange, options:options, ...props}) => (
+  <select {...props} onChange={onChange} defaultValue={value}>
+    {renderOptions(options.toJS(), value)}
+  </select>
 );
 
 const renderOptions = (options, value) => (
-    options.map( option =>
-        <option value={option.value} key={option.value}>{option.label}</option>
-    )
+  options.map( option =>
+      <option value={option.value} key={option.value}>{option.label}</option>
+  )
 );
 
 export default Select;

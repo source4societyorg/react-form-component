5003568586fb827204c8f47c6ce852846e2579aa
Daniel Fredriksen <dfredriksen@cyint.technology>
2017-12-27 16:48:00 -0800

    Added styling options for form layout

--- Field.js
+++ Field.js
@@ -69,7 +69,7 @@ export default function Field({
   }) {
 
   return (
-    <FieldContainer className={'field-container ' + id + '_container'}>
+    <FieldContainer className={'field-container ' + id + '_container ' + props.formLayout}>
       {renderLabel(id, props)}
       {renderFieldBlock(id, props)}
       {renderValidationMessage(id, props)}
@@ -86,6 +86,7 @@ Field.defaultProps = {
   validationMessage: '',
   errorColor: '#cc0000',
   hideLabel: false,
+  formLayout: 'vertical',
 };
 
 Field.propTypes = {
